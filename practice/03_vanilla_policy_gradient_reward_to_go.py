Fixing flattened observation dimension size calculation.

practice/03_vanilla_policy_gradient_reward_to_go.py
@@ -19,7 +19,7 @@ learning_rate = 1e-2
 
 environment = gym.make(environment_name)
 num_actions = environment.action_space.n
-observation_dimensions = sum(environment.observation_space.shape) # How would we deal with other shapes? flatten?
+observation_dimensions = np.prod(environment.observation_space.shape)
 
 
 ################

practice/05_vanilla_policy_gradient_value_function_baseline.py
@@ -22,7 +22,7 @@ value_iterations = 50
 
 environment = gym.make(environment_name)
 num_actions = environment.action_space.n
-observation_dimension = sum(environment.observation_space.shape)
+observation_dimension = np.prod(environment.observation_space.shape)
 
 
 #####################

practice/06_vanilla_policy_gradient_state_action_value_function.py
@@ -20,7 +20,7 @@ action_value_iterations = 50
 ##########################
 
 environment = gym.make(environment_name)
-observation_dimension = sum(environment.observation_space.shape)
+observation_dimension = np.prod(environment.observation_space.shape)
 num_actions = environment.action_space.n
 
 

practice/09_vanilla_policy_gradient_generalized_advantage_estimator.py
@@ -22,7 +22,7 @@ gae_lambda_discount = 0.92
 ##########################
 
 environment = gym.make(environment_name)
-observation_dimension = sum(environment.observation_space.shape)
+observation_dimension = np.prod(environment.observation_space.shape)
 num_actions = environment.action_space.n
 
 

practice/10_vanilla_policy_gradient_continuous_action_space.py
@@ -44,8 +44,8 @@ epochs_to_render = (0,25,num_epochs-1)
 ##########################
 
 environment = gym.make(environment_name)
-observation_dimensions = sum(environment.observation_space.shape)
-action_dimensions = sum(environment.action_space.shape)
+observation_dimensions = np.prod(environment.observation_space.shape)
+action_dimensions = np.prod(environment.action_space.shape)
 
 assert isinstance(environment.observation_space, gym.spaces.Box), "Observation space must be a Box."
 assert isinstance(environment.action_space, gym.spaces.Box), "Action space must be a Box."